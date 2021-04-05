5e1713c569254ec7cf0c8f53855a2d70656379bb
20162014040034 <jonatas.melo@escolar.ifrn.edu.br>
2021-04-05 16:14:15 -0300

    alteração: adicionando componente de contexto ao app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,18 @@
 import "../styles/global.css";
+import { AuthProvider } from '../contexts/auth'
 
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AuthProvider>
       <Head>
         <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
 

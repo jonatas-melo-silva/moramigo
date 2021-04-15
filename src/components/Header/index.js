69f58e5b6b1de12033249b7f5abc1a0701230dc5
20162014040034 <jonatas.melo@escolar.ifrn.edu.br>
2021-04-15 02:27:23 -0300

    alteração: add funcionalidade visualizar-solicitacao-e-interesses

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,70 +1,65 @@
 import styles from "./Header.module.css";
-import {useAuth} from '../../contexts/auth';
+import { useAuth } from "../../contexts/auth";
+import { useInterest } from "../../contexts/interest";
+
 import Link from "next/link";
 
-
-
-
-
 const MyHeader = () => {
-  const { logado, logout, user } = useAuth()
-  // console.log(user)
-  // console.log(logado)
+  const { logado, logout, user } = useAuth();
+  const { listarPendentes } = useInterest();
 
-  const onClick = (event) => {
-    logout()
-  }
+
+
+  const onClickSair = () => {
+    logout();
+  };
   return (
     <>
       <header className={styles.containerHeader}>
         <div className="logo">
           <Link href="/">
             <img src="/img/logo.svg" alt="MorAmigo" />
-            
           </Link>
         </div>
 
         <div className={styles.menu}>
-   
-
           <Link href="/sobrenos">
             <a>Sobre - Nós</a>
           </Link>
           <Link href="/buscar/pessoas">
             <a>Buscar Pessoas</a>
           </Link>
-          {(logado) ? (
+          {logado ? (
             <>
-            <Link href="/interesses/solicitacoes">
-              <a>Meus interesses</a>
-            </Link>
+              <Link href="/interesses/solicitacoes" >
+                <a onClick={listarPendentes}>Meus interesses</a>
+              </Link>
 
-            <Link href="/usuario/login">
-              <a onClick={onClick}>Sair</a>
-            </Link>
-            
-         
-             
-          <Link href="/usuario/perfil">
-            <div id={styles.avatarProfile}>     
-              <p>Rafael<span>Ver perfil</span></p>                   
-              <img src="/img/pessoa1.svg" alt="MorAmigo | Banner" />
-              </div>
-          </Link>
-       
-                
-    
+              <Link
+                href="/usuario/[id]}"
+                as={`/usuario/${user.id}`}
+              >
+                <div id={styles.avatarProfile}>
+                  <p>
+                    {user.nome}<span>Ver perfil</span>
+                  </p>
+                  <img src="/img/pessoa1.svg" alt="MorAmigo | Banner" />
+                </div>
+              </Link>
+
+              <Link href="/usuario/login">
+                <a onClick={onClickSair}>Sair</a>
+              </Link>
 
             </>
           ) : (
             <>
-            
-            <Link href="/usuario/login">
-              <a>Entrar</a>
-            </Link>
-            <Link href="/usuario/registrar">
-              <a>Cadastre-se</a>
-            </Link>
+              <Link href="/usuario/login">
+                <a>Entrar</a>
+              </Link>
+              <Link href="/usuario/registrar">
+                <a>Cadastre-se</a>
+              </Link>
             </>
           )}
         </div>
--- src/pages/interesses/confirmados.js
+++ src/pages/interesses/confirmados.js
@@ -1,122 +1,69 @@
 import styles from "../../styles/pages/confirmados.module.css";
-import Layout from "../../components/Layout"
+import Layout from "../../components/Layout";
+import { useInterest } from "../../contexts/interest";
 
 import Link from "next/link";
 
-const title = 'Moramigo | Meus Interesses'
+const title = "Moramigo | Meus Interesses";
 
 const Confirmados = () => {
+  const { listarPendentes, confirmados } = useInterest();
+
   return (
     <Layout title={title}>
       <div className={styles.container}>
         <div className={styles.links}>
           <nav>
             <Link href="/interesses/solicitacoes">
-              <a>Solicitação de Interesses</a>
+              <a onClick={listarPendentes}>Solicitação de Interesses</a>
             </Link>
-            <a className={styles.active}>
-              Interesses Confirmados
-            </a>
+            <a className={styles.active}>Interesses Confirmados</a>
           </nav>
         </div>
 
         <div className={styles.cards}>
-          <div className={styles.card}>
-            <section>
-              <div className={styles.perfil}>
-                <div>
-                  <img src="/img/pessoa1.svg" alt="MorAmigo | Banner" />
-                </div>
-                <div>
-                  <h1>Rafael Rufino</h1>
-                  <p>25 Anos</p>
-                </div>
-              </div>
-              <div className={styles.verPerfil}>
-                <Link href="/perfil">
-                  <a>Ver Perfil</a>
-                </Link>
-              </div>
+          {console.log(confirmados)}
+          {confirmados.map((c) => (
+            <>
+              <div className={styles.card} key={c.origem.id}>
+                <section>
+                  <div className={styles.perfil}>
+                    <div>
+                      <img src="/img/pessoa1.svg" alt="MorAmigo | Banner" />
+                    </div>
+                    <div>
+                      <h1>{c.origem.nome}</h1>
+                      <p>{c.origem.idade}</p>
+                    </div>
+                  </div>
+                  <div className={styles.verPerfil}>
+                    <Link
+                      href="/usuario/perfil/[id]}"
+                      as={`/usuario/perfil/${c.origem.id}`}
+                    >
+                      <a>Ver Perfil</a>
+                    </Link>
+                  </div>
 
-              <span>
-                Olá moramigo, estou precisando de um colega para dividir uma
-                moradia.
-              </span>
+                  <span>{c.origem.descricao}</span>
 
-              <span> Bairro: Alecrim</span>
-              <div className={styles.button}>
-                <button className={styles.inciarConversa}>
-                  <Link href="/perfil">
-                    <a>Iniciar Conversa</a>
-                  </Link>
-                </button>
+                  <span>
+                    Bairro:{" "}
+                    {c.origem.restricoes.localidades.map((local) => (
+                      <span key={local.nome}>{local.nome}</span>
+                    ))}
+                  </span>
+                  <div className={styles.button}>
+                    <button className={styles.inciarConversa}>
+                      <Link href="/perfil">
+                        <a>Iniciar Conversa</a>
+                      </Link>
+                    </button>
+                  </div>
+                </section>
               </div>
-            </section>
-          </div>
-          <div className={styles.card}>
-            <section>
-              <div className={styles.perfil}>
-                <div>
-                  <img src="/img/pessoa1.svg" alt="MorAmigo | Banner" />
-                </div>
-                <div>
-                  <h1>Rafael Rufino</h1>
-                  <p>25 Anos</p>
-                </div>
-              </div>
-              <div className={styles.verPerfil}>
-                <Link href="/perfil">
-                  <a>Ver Perfil</a>
-                </Link>
-              </div>
-
-              <span>
-                Olá moramigo, estou precisando de um colega para dividir uma
-                moradia.
-              </span>
-
-              <span> Bairro: Alecrim</span>
-              <div className={styles.button}>
-                <button className={styles.inciarConversa}>
-                  <Link href="/perfil">
-                    <a>Iniciar Conversa</a>
-                  </Link>
-                </button>
-              </div>
-            </section>
-          </div>
-          <div className={styles.card}>
-            <section>
-              <div className={styles.perfil}>
-                <div>
-                  <img src="/img/pessoa1.svg" alt="MorAmigo | Banner" />
-                </div>
-                <div>
-                  <h1>Rafael Rufino</h1>
-                  <p>25 Anos</p>
-                </div>
-              </div>
-              <div className={styles.verPerfil}>
-                <Link href="/perfil">
-                  <a>Ver Perfil</a>
-                </Link>
-              </div>
-
-              <span>
-                Olá moramigo, estou precisando de um colega para dividir uma
-                moradia.
-              </span>
-
-              <span> Bairro: Alecrim</span>
-              <div className={styles.button}>
-                <button className={styles.inciarConversa}>
-                  <Link href="/perfil">
-                    <a>Iniciar Conversa</a>
-                  </Link>
-                </button>
-              </div>
-            </section>
-          </div>
+            </>
+          ))}
         </div>
       </div>
     </Layout>

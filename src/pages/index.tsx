65987cc6924fc8dde463a00d77dc335af82e5d28
20162014040034 <jonatas.melo@escolar.ifrn.edu.br>
2021-07-21 15:45:48 -0300

    change: configurando prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import { perfilFetch } from "../hooks/perfilFetch";
-import { memo } from "react";
+import { perfilFetch } from '../hooks/perfilFetch';
+import { memo } from 'react';
 
-import Layout from "../components/Layout";
-import Banner from "../components/home/Banner";
-import Cards from "../components/search/Cards";
-import InforHome from "../components/home/InforHome";
-import TextContainer from "../components/home/TextContainer";
-import { Paginacao } from "../components/Paginacao";
+import Layout from '../components/Layout';
+import Banner from '../components/home/Banner';
+import Cards from '../components/search/Cards';
+import InforHome from '../components/home/InforHome';
+import TextContainer from '../components/home/TextContainer';
+import { Paginacao } from '../components/Paginacao';
 
-const title = "Moramigo";
+const title = 'Moramigo';
 
 const Home = () => {
-  const { data } = perfilFetch("busca/");
+  const { data } = perfilFetch('busca/');
   return (
     <Layout title={title}>
       <Banner />

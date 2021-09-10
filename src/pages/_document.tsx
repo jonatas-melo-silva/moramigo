2f4533ec9884e974ee626ce25c1b72556f13681a
jonatas-melo-silva <jonatas891024@gmail.com>
2021-09-10 17:01:42 -0300

    :ambulance: Adicionado config para requisiçoes http

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,9 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head/>
+
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+        
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
